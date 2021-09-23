5f9621603aeb516687f219a8094871882c4a84b0
joewanko <joewanko@users.noreply.github.com>
2021-09-23 15:53:59 -0400

    Initial commit

---
+++ src/markdownLexer/defaults.js
@@ -0,0 +1,21 @@
+export default {
+  baseUrl: null,
+  breaks: false,
+  extensions: null,
+  gfm: true,
+  headerIds: true,
+  headerPrefix: "",
+  highlight: null,
+  langPrefix: "language-",
+  mangle: true,
+  pedantic: false,
+  renderer: null,
+  sanitize: false,
+  sanitizer: null,
+  silent: false,
+  smartLists: false,
+  smartypants: false,
+  tokenizer: null,
+  walkTokens: null,
+  xhtml: false
+};
